ede4161b12807cd78b7e3886cbb911b2ccd5250b
LorenzoB <59319678+AndreaRe9@users.noreply.github.com>
2023-11-09 17:15:20 +0100

    New tables added

--- code/dataset/init.sql
+++ code/dataset/init.sql
@@ -1,64 +1,64 @@
 CREATE TABLE IF NOT EXISTS student (
-  id varchar(10) PRIMARY KEY,
-  surname varchar(30) not NULL,
-  name varchar(30) NOT NULL,
+  id varchar(30) PRIMARY KEY,
+  surname varchar(100) not NULL,
+  name varchar(100) NOT NULL,
   gender varchar(1) NOT NULL,
   nationality varchar(20) NOT NULL,
-  email varchar(30) NOT NULL,
+  email varchar(50) NOT NULL,
   COD_DEGREE varchar(10) NOT NULL,
   ENROLLMENT_YEAR int NOT NULL
  );
 
  
  CREATE TABLE IF NOT EXISTS TEACHER (
-  id varchar(10) PRIMARY KEY,
-  surname varchar(30) not NULL,
-  name varchar(30) NOT NULL,
-  email varchar(30) NOT NULL,
+  id varchar(30) PRIMARY KEY,
+  surname varchar(100) not NULL,
+  name varchar(100) NOT NULL,
+  email varchar(50) NOT NULL,
   COD_GROUP varchar(10) NOT NULL,
   COD_DEPARTMENT varchar(10) NOT NULL
  );
  
  CREATE TABLE IF NOT EXISTS CO_SUPERVISOR(
-   id varchar(10) PRIMARY KEY,
-   surname varchar(30) not NULL,
-   name varchar(30) NOT NULL,
-   email varchar(30) NOT NULL
+   id varchar(30) PRIMARY KEY,
+   surname varchar(100) not NULL,
+   name varchar(100) NOT NULL,
+   email varchar(50) NOT NULL
   );
 
   CREATE TABLE IF NOT EXISTS student (
-  id varchar(10) PRIMARY KEY,
-  surname varchar(30) not NULL,
-  name varchar(30) NOT NULL,
+  id varchar(30) PRIMARY KEY,
+  surname varchar(100) not NULL,
+  name varchar(100) NOT NULL,
   gender varchar(1) NOT NULL,
-  nationality varchar(20) NOT NULL,
-  email varchar(30) NOT NULL,
+  nationality varchar(30) NOT NULL,
+  email varchar(50) NOT NULL,
   COD_DEGREE varchar(10) NOT NULL,
   ENROLLMENT_YEAR int NOT NULL
  );
 
  
  CREATE TABLE IF NOT EXISTS TEACHER (
-  id varchar(10) PRIMARY KEY,
-  surname varchar(30) not NULL,
-  name varchar(30) NOT NULL,
-  email varchar(30) NOT NULL,
+  id varchar(30) PRIMARY KEY,
+  surname varchar(100) not NULL,
+  name varchar(100) NOT NULL,
+  email varchar(50) NOT NULL,
   COD_GROUP varchar(10) NOT NULL,
   COD_DEPARTMENT varchar(10) NOT NULL
  );
  
  CREATE TABLE IF NOT EXISTS CO_SUPERVISOR(
-   id varchar(10) PRIMARY KEY,
-   surname varchar(30) not NULL,
-   name varchar(30) NOT NULL,
-   email varchar(30) NOT NULL
+   id varchar(30) PRIMARY KEY,
+   surname varchar(100) not NULL,
+   name varchar(100) NOT NULL,
+   email varchar(50) NOT NULL
   );
  
  CREATE TABLE IF NOT EXISTS THESIS_PROPOSAL (
    id serial PRIMARY KEY,
    title varchar(50) NOT NULL,
-   SUPERVISOR_ID varchar(10) NOT NULL,
-   COSUPERVISOR_ID varchar(10) NOT NULL,
+   SUPERVISOR_id varchar(30) NOT NULL,
+   COSUPERVISOR_id varchar(30) NOT NULL,
    keywords varchar(50) not NULL,
    type varchar(20) NOT NULL,
    groups varchar(20) NOT NULL,         /* SHOULD BE CLARIFIED */
@@ -71,6 +71,27 @@ CREATE TABLE IF NOT EXISTS student (
    status varchar(10) not NULL,
    FOREIGN KEY (SUPERVISOR_ID) references TEACHER(id),
    FOREIGN KEY (COSUPERVISOR_ID) references CO_SUPERVISOR(id)
+  );
+
+CREATE TABLE IF NOT EXISTS THESIS_APPLICATION (
+  student_id varchar(30) NOT NULL,
+  thesis_id serial NOT NULL,
+  status varchar(10) NOT NULL DEFAULT 'idle',
+  cv_uri varchar(250),
+  FOREIGN KEY (student_id) references student(id),
+  FOREIGN KEY (thesis_id) references THESIS_PROPOSAL(id),
+  PRIMARY KEY (student_id, thesis_id)
+  );
+
+CREATE TABLE IF NOT EXISTS CAREER (
+  student_id varchar(30) NOT NULL,
+  COD_COURSE varchar(15) NOT NULL,
+  TITLE_COURSE varchar(30) NOT NULL,
+  CFU int NOT NULL,
+  GRADE int NOT NULL,
+  ddate date NOT NULL,
+  FOREIGN KEY (student_id) references student(id),
+  PRIMARY KEY (student_id, COD_COURSE)
   );
   
   
