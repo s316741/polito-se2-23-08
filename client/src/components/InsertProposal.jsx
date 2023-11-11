4a731180b52711296171ef5f326bff4f6e5a781e
Sahand <51256022+SahandGhavami@users.noreply.github.com>
2023-11-11 12:54:34 +0000

    Change insertproposal tailwind to bootstrap class

--- client/src/components/InsertProposal.jsx
+++ client/src/components/InsertProposal.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import React from "react";
-import { WithContext as ReactTags } from "react-tag-input";
+import { TagsInput } from "react-tag-input-component";
 import "../App.css";
 
 function handleSubmit(e) {
@@ -15,106 +15,139 @@ const KeyCodes = {
 const delimiters = [KeyCodes.comma, KeyCodes.enter];
 
 function InsertProposal() {
-  const [tags, setTags] = React.useState([]);
+  const [selected, setSelected] = React.useState([]);
 
-  // Method to delete tag from Array
-  const handleDelete = (i) => {
-    setTags(tags.filter((tag, index) => index !== i));
-  };
-
-  // Method to Add tag into Array
-  const handleAddition = (tag) => {
-    setTags([...tags, tag]);
-  };
   return (
     <>
-      <Navbar></Navbar>
-      <div className="py-2 px-4 mx-auto max-w-screen-md">
+      <div className="py-2 px-4 mx-auto max-md">
         <h2 className="mb-4 text-4xl font-extrabold text-center text-gray-900">
           Proposal Page
         </h2>
-        <p className="mb-4 font-light text-center text-gray-500 sm:text-xl">
-        </p>
+        <p className="mb-4 font-light text-center text-gray-500 fs-5"></p>
         <form
-          className="w-1/1 mx-auto p-4 bg-white rounded shadow-md mt-10"
+          className="container mt-5 p-4 bg-light rounded shadow mt-10"
           method="post"
           onSubmit={handleSubmit}
         >
-          <label>
-            Proposal Title:
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              Proposal Title:
+            </label>
             <input
-              className="border rounded px-3 py-2 mt-1 mb-2 w-full"
+              type="text"
+              className="form-control"
+              id="title"
               name="title"
+              placeholder="Enter proposal title"
             />
-          </label>
-          <label>
-            Description:
-            <input
-              className="border rounded px-3 py-2 mt-1 mb-2 w-full h-20 "
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description:
+            </label>
+            <textarea
+              className="form-control border rounded px-3 py-2 mt-1 mb-2"
+              id="description"
               name="description"
-            />
-          </label>
-          <label>
-            Required knowledge:
+              placeholder="Enter proposal description"
+              rows="4"
+            ></textarea>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="knowledge" className="form-label">
+              Required knowledge:
+            </label>
             <input
-              className="border rounded px-3 py-2 mt-1 mb-2 w-full"
+              type="text"
+              className="form-control border rounded px-3 py-2 mt-1 mb-2"
+              id="knowledge"
               name="knowledge"
+              placeholder="Enter required knowledge"
             />
-          </label>
-          <label className="flex flex-wrap -mx-2">
-            <div className="w-1/3 px-2">
-              <label className="block">Level:</label>
-              <select className="border rounded px-3 py-2 w-full" name="level">
+          </div>
+
+          <div className="row g-3">
+            <div className="col-md-4">
+              <label htmlFor="level" className="form-label block">
+                Level:
+              </label>
+              <select
+                className="form-select border rounded px-3 py-2"
+                id="level"
+                name="level"
+              >
                 <option value="Bachelor">Bachelor</option>
                 <option value="Master">Master</option>
                 <option value="PhD">PhD</option>
               </select>
             </div>
-            <div className="w-1/3 px-2">
-              <label className="block">CdS /programmes:</label>
+
+            <div className="col-md-4">
+              <label htmlFor="programmes" className="form-label block">
+                CdS /programmes:
+              </label>
               <input
-                className="border rounded px-3 py-2 w-full"
+                type="text"
+                className="form-control border rounded px-3 py-2"
+                id="programmes"
                 name="programmes"
               />
             </div>
-            <div className="w-1/3 px-2">
-              <label className="block">Groups:</label>
+
+            <div className="col-md-4">
+              <label htmlFor="groups" className="form-label block">
+                Groups:
+              </label>
               <input
-                className="border rounded px-3 py-2 w-full"
+                type="text"
+                className="form-control border rounded px-3 py-2"
+                id="groups"
                 name="groups"
               />
             </div>
-            <div className="w-1/2 px-2">
-              <label className="block">Type:</label>
-              <input className="border rounded px-3 py-2 w-full" name="type" />
-            </div>
-            <div className="w-1/2 px-2">
-              <label className="block">Deadline:</label>
+
+            <div className="col-md-6">
+              <label htmlFor="type" className="form-label block">
+                Type:
+              </label>
               <input
-                className="border rounded px-3 py-2 w-full"
-                name="deadline"
-                type="date"
+                type="text"
+                className="form-control border rounded px-3 py-2"
+                id="type"
+                name="type"
               />
             </div>
-          </label>
-          <div className="w-full">
-            <label className="block">Keywords:</label>
-            <div className="border rounded px-3 py-2 mt-1 w-full">
-              <ReactTags
-                tags={tags}
-                delimiters={delimiters}
-                handleDelete={handleDelete}
-                handleAddition={handleAddition}
-                inputFieldPosition="bottom"
-                autocomplete
-                allowDragDrop={false}
+
+            <div className="col-md-6">
+              <label htmlFor="deadline" className="form-label block">
+                Deadline:
+              </label>
+              <input
+                type="date"
+                className="form-control border rounded px-3 py-2"
+                id="deadline"
+                name="deadline"
               />
             </div>
           </div>
-          <div className="text-right mt-4">
+
+          <div className="mb-3">
+            <label htmlFor="keywords" className="form-label block">
+              Keywords:
+            </label>
+              <TagsInput
+                value={selected}
+                onChange={setSelected}
+                name="fruits"
+                placeHolder="enter fruits"
+              />
+          </div>
+
+          <div className="d-flex justify-content-end mt-4">
             <button
               type="submit"
-              className="bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 mt-1 mb-2 rounded"
+              className="btn btn-success py-2 px-4 mt-1 mb-2 rounded"
             >
               Confirm
             </button>
